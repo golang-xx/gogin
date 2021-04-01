fc13462f7acca18b5d2b8c2f5f760d0902bd16b0
liliang8858 <liliang8858@126.com>
2021-04-01 21:51:57 +0800

    first time

--- ginweb01/main.go
+++ ginweb01/main.go
@@ -1,10 +1,29 @@
 package main
 import "github.com/gin-gonic/gin"
 
+type UserController struct {
+	*gin.Engine
+}
+// 构造函数
+func NewUserController(e *gin.Engine) *UserController {
+	return &UserController{e}
+}
+
+// 获取用户 业务逻辑
+func (this *UserController)GetUser() gin.HandlerFunc {
+	return func(ctx *gin.Context) {
+		ctx.JSON(200,gin.H{
+			"data":"hello world",
+		})
+	}
+}
+
+func (this *UserController) Router() {
+	this.Handle("GET","/",this.GetUser())
+}
+
 func main(){
 	r := gin.Default()
-	r.GET("/",func(c *gin.Context){
-		c.JSON(200,gin.H{"message":"pong",})
-	})
+	NewUserController(r).Router()
 	r.Run(":8091")
 }
